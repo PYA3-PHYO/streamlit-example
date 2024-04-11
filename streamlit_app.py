917e2df7f1cb1708923684db1dcd61b855cb714e
Patrick <100300769+PYA3-PHYO@users.noreply.github.com>
2024-04-11 15:09:38 +0000

    ver1(not showing result img)

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,39 +2,44 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+from io import StringIO
+from ultralytics import YOLO
+import cv2
+from PIL import Image 
 
-"""
-# Welcome to Streamlit!
+st.title('This is Aurora...')
+st.title('Vehicle Type detection Model :blue[1.0] :sunglasses:...')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.caption('Please upload an image to  identify any Vehicle quickly and easily with our AI-Powered toolvehicles types..')
+st.caption('Oue Model can predict 5 vehicle classes: Bus, car, Ambulance, Truck, and Motorcycle')
+st.caption('Powered by advanced artificial intelligence YOLOv8...')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# function to convert file buffer to cv2 image
+def create_opencv_image_from_stringio(img_stream, cv2_img_flag=1):
+    return cv2.imread(img_stream, cv2_img_flag)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+model  = YOLO('runs/best.pt')
+uploaded_file = st.file_uploader("Choose a file", type=["png", "jpg", "jpeg"])
+if uploaded_file is not None:
+    # 1) image will converted to cv2 image
+    str_img_file = str(uploaded_file)
+    open_cv_image = create_opencv_image_from_stringio(str_img_file)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+if st.button('Detect..'):
+    st.header('Here is your Result!', divider='rainbow')
+    # 2) pass image to the model to get the detection result
+    result_img = model(open_cv_image, imgsz=320, conf=0.5)
+    # # 3) extract numpy array from the list
+    # result_array = result_img[0].numpy()
+    # # 4) convert array to PIL image
+    # pil_image = Image.fromarray(result_img)
+    
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+  
+    if result_img is not None:
+        with st.spinner('Wait for it...'):
+            st.image(result_img, channels="BGR")
+
+
