18ef720a74319a079bd70589c612493ad6bffd93
Patrick <100300769+PYA3-PHYO@users.noreply.github.com>
2024-04-12 20:30:00 +0000

    demo ver

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,40 +6,45 @@ from io import StringIO
 from ultralytics import YOLO
 import cv2
 from PIL import Image 
+from datetime import datetime
+import os
 
 st.title('This is Aurora...')
-st.title('Vehicle Type detection Model :blue[1.0] :sunglasses:...')
+st.title('Vehicle Type Detection Model :blue[1.0] :sunglasses:...')
 
 st.caption('Please upload an image to  identify any Vehicle quickly and easily with our AI-Powered toolvehicles types..')
 st.caption('Oue Model can predict 5 vehicle classes: Bus, car, Ambulance, Truck, and Motorcycle')
 st.caption('Powered by advanced artificial intelligence YOLOv8...')
 
-# function to convert file buffer to cv2 image
-def create_opencv_image_from_stringio(img_stream, cv2_img_flag=1):
-    return cv2.imread(img_stream, cv2_img_flag)
+# function to convert file to cv2 image
+def create_opencv_image_from_stringio(img_stream):
+    return cv2.imread(img_stream)
 
 model  = YOLO('runs/best.pt')
 uploaded_file = st.file_uploader("Choose a file", type=["png", "jpg", "jpeg"])
 if uploaded_file is not None:
-    # 1) image will converted to cv2 image
-    str_img_file = str(uploaded_file)
-    open_cv_image = create_opencv_image_from_stringio(str_img_file)
-
-
+            img = Image.open(uploaded_file)
+            st.image(img, caption='Uploaded Image')
+            string_img = str(uploaded_file)
+            cv_image = create_opencv_image_from_stringio(string_img)
+            ts = datetime.timestamp(datetime.now())
+            imgpath = os.path.join('data/uploads', str(ts)+uploaded_file.name)
+            outputpath = os.path.join(
+                 'data/outputs', os.path.basename(imgpath))
+            with open(imgpath, mode="wb") as f:
+                f.write(uploaded_file.getbuffer())
 
 if st.button('Detect..'):
     st.header('Here is your Result!', divider='rainbow')
     # 2) pass image to the model to get the detection result
-    result_img = model(open_cv_image, imgsz=320, conf=0.5)
-    # # 3) extract numpy array from the list
-    # result_array = result_img[0].numpy()
-    # # 4) convert array to PIL image
-    # pil_image = Image.fromarray(result_img)
-    
-
-  
-    if result_img is not None:
-        with st.spinner('Wait for it...'):
-            st.image(result_img, channels="BGR")
-
+    with st.spinner('Wait for it...'):
+        # pred = model(imgpath)
+        # pred.orig_img
+        result_img = model(imgpath, imgsz=640, conf=0.25)
+        # result_img_array = result_img.orig_img
+        arry_img_result = result_img[0].plot()
+        
+        # # Predictions
+        image_rgb = cv2.cvtColor(arry_img_result,cv2.COLOR_BGR2RGB)
+        st.image(image_rgb, caption='Model Prediction(s)')
 
